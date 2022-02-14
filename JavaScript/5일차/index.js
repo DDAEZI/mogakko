a72cf9a67c689a85d420b4376e2353ba5ddfe9c3
DZ <78488493+DDAEZI@users.noreply.github.com>
2022-02-14 20:56:22 +0900

    5일차

---
+++ JavaScript/5일차/index.js
@@ -0,0 +1,10 @@
+var num = 3;
+var str = "안녕하세용";
+var TorF = true;
+var D = {k:'v'};
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof TorF);
+console.log(typeof D);
+console.log(typeof a);
