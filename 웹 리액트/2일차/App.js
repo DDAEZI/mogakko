11f2c74f53c88dc539339d913f0bf2c96817b7e2
DZ <78488493+DDAEZI@users.noreply.github.com>
2022-03-08 17:06:30 +0900

    2일차

---
+++ 웹 리액트/2일차/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Desc from './Desc';
+
+function App() {
+  return (
+    <div className="App">
+    <Desc />
+    </div>
+  );
+}
+
+export default App;
