7941f0649b69658bd922c73d3db6e12d95b5ba68
DZ <78488493+DDAEZI@users.noreply.github.com>
2022-03-09 15:07:49 +0900

    3일차

---
+++ 웹 리액트/3일차/Desc.js
@@ -0,0 +1,11 @@
+import styles from './Desc.module.css';
+
+function Desc() {
+    return (<div className="Box">
+        <h1 className={styles.h1}>MARK</h1>
+        <p>'MARK'는 크롬, 사파리 등 다양한 환경에서 생겨나는 즐겨찾기들을 한 곳으로 모아주는 역할을 해요! <br/>공유를 통한 등록 또는 직접 등록을 통해 등록할 수 있어요.</p>
+    </div>)
+}
+
+
+export default Desc;
